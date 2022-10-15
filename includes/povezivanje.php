1dfe9dab0b6dfca175e2387fc1b3ed9d238958c1
Damjan Pavlica <mudroljub@gmail.com>
2022-10-16 01:30:26 +0200

    use env

--- includes/povezivanje.php
+++ includes/povezivanje.php
@@ -3,6 +3,6 @@
 /* enable error reporting */
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-$mysqli = new mysqli('', 'gorran02', 'pavlica21', 'znaci');
+$mysqli = new mysqli('', getenv('MYSQL_UN'), getenv('MYSQL_PW'), getenv('MYSQL_DB'));
 
 $mysqli->set_charset('utf8');
