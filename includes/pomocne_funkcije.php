e68d560ff93242b96d6900fd35a513c0932993d2
Damjan Pavlica <mudroljub@gmail.com>
2022-10-17 00:07:36 +0200

    m

--- includes/pomocne_funkcije.php
+++ includes/pomocne_funkcije.php
@@ -1,24 +1,24 @@
 <?php
 
-function je_li_slobodan($grad, $dan, $mesec, $godina)
+function je_li_aktivno($entitet_id, $dan, $mesec, $godina)
 {
 	$dan_rata = 10497 + strtotime($godina . "-" . $mesec . "-" . $dan) / 86400;
 	$konekcija = mysqli_connect("", getenv('MYSQL_UN'), getenv('MYSQL_PW'), getenv('MYSQL_DB'));
 	mysqli_set_charset($konekcija, 'utf8');
 
-	$upit_sx = "SELECT * FROM eventu WHERE ko=" . $grad . " order by kadyy,kadmm,kadd;";
-	$rezultat_sx = mysqli_query($konekcija, $upit_sx);
+	$upit = "SELECT * FROM eventu WHERE ko=$entitet_id order by kadyy, kadmm, kadd;";
+	$rezultat = mysqli_query($konekcija, $upit);
 	$ishod = 2;
-	while ($red_sx = mysqli_fetch_assoc($rezultat_sx)) {
-		if (!isset($red_sx)) {
+	while ($red = mysqli_fetch_assoc($rezultat)) {
+		if (!isset($red)) {
 			break;
 		}
-		$kko = $red_sx['ko'];
-		$dan_akcije = 10497 + strtotime($red_sx['kadyy'] . "-" . $red_sx['kadmm'] . "-" . $red_sx['kadd']) / 86400;
+		$kko = $red['ko'];
+		$dan_akcije = 10497 + strtotime($red['kadyy'] . "-" . $red['kadmm'] . "-" . $red['kadd']) / 86400;
 		if ($dan_akcije > $dan_rata) {
 			break;
 		}
-		$ishod = $red_sx['sta'];
+		$ishod = $red['sta'];
 		if ($ishod > 2) {
 			$ishod = $ishod - 2;
 		}
@@ -33,6 +33,7 @@ function string_divizije($dan, $mesec, $godina)
 
 	$i = 0;
 	$nazivi = array();
+	// selektuje sve nemačke divizije u jugoslaviji
 	$upit = "SELECT distinct(eventu.ko), entia.id as entia_id, entia.naziv as entia_naziv FROM entia INNER JOIN eventu ON entia.id=eventu.ko WHERE entia.prip=11;";
 	$rezultat = mysqli_query($konekcija, $upit);
 	while ($red = mysqli_fetch_assoc($rezultat)) {
@@ -42,9 +43,8 @@ function string_divizije($dan, $mesec, $godina)
 	}
 
 	$strg = "";
-
 	for ($i = 0; $i < count($nazivi); $i++) {
-		$status = je_li_slobodan($idovi[$i], $dan, $mesec, $godina);
+		$status = je_li_aktivno($idovi[$i], $dan, $mesec, $godina);
 		if ($status == 1) {
 			$stil = " class=bigTitle_c";
 		} else {
