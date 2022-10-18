20fd3afa86c4dbb2160d3da71d6ae3635d9b1a5c
Damjan Pavlica <mudroljub@gmail.com>
2022-10-19 00:14:25 +0200

    m

--- na-danasnji-dan.php
+++ na-danasnji-dan.php
@@ -3,15 +3,15 @@ $naslov = "Na današnji dan";
 include "includes/header.php";
 include "includes/pomocne_funkcije.php";
 
-$ddd = $_POST['dan'] ?: date("d");
-$mmm = $_POST['mesec'] ?: date("m");
+$ddd = $_GET['dan'] ?: date("d");
+$mmm = $_GET['mesec'] ?: date("m");
 ?>
 
 <h2>Hronologija NOR-a: na današnji dan</h2>
 
-<form method="POST" id="f1" action="<?php echo $_SERVER['SCRIPT_NAME']; ?>">
-	dan: <input type="text" value="<?php echo $ddd; ?>" name="dan">
-	mesec: <input type="text" value="<?php echo $mmm; ?>" name="mesec">
+<form method="GET" id="f1" action="<?php echo $_SERVER['SCRIPT_NAME']; ?>">
+	dan: <input type="number" value="<?php echo $ddd; ?>" name="dan">
+	mesec: <input type="number" value="<?php echo $mmm; ?>" name="mesec">
 	<input type="submit" value="pronadji">
 </form>
 
@@ -23,7 +23,7 @@ $mmm = $_POST['mesec'] ?: date("m");
 	for ($godina = $pocni_od; $godina < $pocni_od + 4; $godina++) {
 		$divizije = string_divizije($ddd, $mmm, $godina);
 		echo "<h1 style=margin-bottom:0>$godina</h1>
-			<h3>Nemačke divizije na teritoriji Jugoslavije $ddd.$mmm.$godina:</h3>$divizije";
+			<h3>Nemačke divizije na teritoriji Jugoslavije dana $ddd.$mmm.$godina:</h3>$divizije";
 	}
 	?>
 </div>
