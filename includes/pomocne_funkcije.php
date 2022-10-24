5530cdef83ab7134846491872c3589aa3a83e7c3
Damjan Pavlica <mudroljub@gmail.com>
2022-10-24 13:56:01 +0200

    refactor

--- includes/pomocne_funkcije.php
+++ includes/pomocne_funkcije.php
@@ -26,14 +26,12 @@ function je_li_aktivno($entitet_id, $dan, $mesec, $godina)
 	return $status == 1;
 }
 
-function string_divizije($dan, $mesec, $godina)
-{
+function get_aktivne_divizije($dan, $mesec, $godina) {
 	$konekcija = mysqli_connect("", getenv('MYSQL_UN'), getenv('MYSQL_PW'), getenv('MYSQL_DB'));
 	mysqli_set_charset($konekcija, 'utf8');
 
 	$divizije = [];
 
-	// selektuje nemačke divizije u jugoslaviji
 	$upit = "SELECT distinct(eventu.ko) AS id, entia.naziv, entia.slug FROM entia 
 	INNER JOIN eventu ON eventu.ko=entia.id 
 	WHERE entia.prip=11";
@@ -41,20 +39,24 @@ function string_divizije($dan, $mesec, $godina)
 	$rezultat = mysqli_query($konekcija, $upit);
 
 	while ($red = mysqli_fetch_assoc($rezultat)) {
-		$data[] = $red['id']; // 0
-		$data[] = $red['naziv'];	// 1
-		$data[] = $red['slug'];		// 2
-		$data[] = je_li_aktivno($red['id'], $dan, $mesec, $godina); // 3
-		$divizije[] = $data;
-		unset($data);
+		if (je_li_aktivno($red['id'], $dan, $mesec, $godina)) {
+			$data[] = $red['id']; // 0
+			$data[] = $red['naziv'];	// 1
+			$data[] = $red['slug'];		// 2
+			$divizije[] = $data;
+		}
 	}
+	return $divizije;
+}
 
+function string_divizije($dan, $mesec, $godina)
+{
+
+	$divizije = get_aktivne_divizije($dan, $mesec, $godina);
 	$strg = "";
 	for ($i = 0; $i < count($divizije); $i++) {
 		$divizija = $divizije[$i];
-		if ($divizija[3]) {
-			$strg = $strg . "<a href=odrednica.php?slug=$divizija[2]>" . $divizija[1] . "</a> * ";
-		}
+		$strg = $strg . "<a href=odrednica.php?slug=$divizija[2]>" . $divizija[1] . "</a> * ";
 	}
 	return $strg;
 }
