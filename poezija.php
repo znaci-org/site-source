b237a934bd1abd09b9e396039e43862ab5e218ae
Damjan Pavlica <mudroljub@gmail.com>
2024-06-07 19:35:00 +0200

    copic

--- poezija.php
+++ poezija.php
@@ -19,12 +19,19 @@ include "includes/header.php";
 
     <ul>
         <li><a href="poezija/cetvrta-ofanziva.php">Četvrta ofanziva</a></li>
+        <li><a href="poezija/lekcije-iz-zemljopisa.php">Lekcije iz zemljopisa</a></li>
     </ul>
 
     <h3>Slavko Vukosavljević</h3>
 
     <ul>
         <li><a href="poezija/kadinjaca.php">Kadinjača</a></li>
+    </ul>
+
+    <h3>Narodne pesme</h3>
+
+    <ul>
+        <li><a href="poezija/uspavanka.php">Uspavanka</a></li>
     </ul>
 
 </div>
---
+++ poezija/uspavanka.php
@@ -0,0 +1,21 @@
+<?php
+$naslov = "Poezija NOB";
+include "../includes/header.php";
+?>
+
+<a href="/poezija.php">◀ Poezija NOB</a>
+
+<h1>Uspavanka</h1>
+
+<p>Spavaj mirno, čedo moje<br>
+Petokraka zvijezda sja<br>
+Nećeš biti hljeba gladan<br>
+Kao što sam bila ja</p>
+
+<p>Tvoj je otac poginuo<br>
+U šumi je njegov grob<br>
+Branio je tebe, sine<br>
+Da ne budeš tuđi rob.</p>
+
+
+<?php include "../includes/footer.php"; ?>
