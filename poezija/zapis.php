2aacf10976ecd6a2eb9ec1fe167e12e8375239b8
Damjan Pavlica <mudroljub@gmail.com>
2024-06-11 18:59:08 +0200

    Banjević

---
+++ poezija/zapis.php
@@ -0,0 +1,59 @@
+<?php
+$naslov = "Poezija NOB";
+include "../includes/header.php";
+?>
+
+<a href="/poezija.php">◀ Poezija NOB</a>
+
+<h1>Mirko Banjević - Zapis</h1>
+
+<p>
+  U crkvi Gospe od škrijela, na ostrvcu pred Perastom, neki je Njemac 1944. urezao ovaj distih:<br />
+  - <i>Wie oft dass ich hier gestanden und gedacht dass mich der Teufel nach Montenegro gebracht.</i><br />
+  (Koliko sam puta ovđe stajao i razmišljao koji me u Crnu Goru donese đavo).<br />
+</p>
+
+<p>
+  O, ko me dovede u taj kamen siv<br />
+  đe se i val morski izubija živ,<br />
+  <br />
+  a ni paklu ovđe nije sličan trag,<br />
+  po kome skapava i sam ljuti vrag!<br />
+  <br />
+  Ove mračne zemlje, đe si nezvan gost,<br />
+  staze su – zasjede, prelaz – srušen most.<br />
+  <br />
+  Ni kost moju neće iznijeti vran<br />
+  iz te Gore Crne, crnji sviće dan.<br />
+  <br />
+  I ko ovđe uđe, susilan i jak,<br />
+  ne vraća se više, poniće ga mrak.<br />
+  <br />
+  Bezdano mu more – otpočinak crn,<br />
+  a postelja – škriljci i otrovni trn.<br />
+  <br />
+  I vrisak mi majke biće žalan pjev<br />
+  mome pustom glasu, kad padnem pod gnjev<br />
+  <br />
+  podmukloga krša, đe kopamo rov,<br />
+  đe i krik divljači nemušti je zov,<br />
+  <br />
+  od koga se zebe – trudi su mi led –<br />
+  i sve tu što soči samo toči jed,<br />
+  <br />
+  samo je osveta i buntovnik sam:<br />
+  ovđe ugušimo – tamo bukne plam!<br />
+  <br />
+  Ah u noći mojoj, u beznadan čas,<br />
+  bdim, daleki dragi, misleći na vas.<br />
+  <br />
+  I gledam zloslutnih ovih brda zub<br />
+  kako mi zakida na vidiku rub.<br />
+  <br />
+  Đe ja kruto stadoh – žme mi srce strah.<br />
+  Nek sto živih munja raznesu i prah<br />
+  <br />
+  toga ko me spremi ovom kraju zlom,<br />
+  iz kog nikad više neću viđet dom!<br />
+  <br />
+</p>
