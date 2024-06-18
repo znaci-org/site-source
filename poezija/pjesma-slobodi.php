a2375c5537d15494e3e36f67e49133ed0a459364
Damjan Pavlica <mudroljub@gmail.com>
2024-06-18 17:49:17 +0200

    m

--- poezija/pjesma-slobodi.php
+++ poezija/pjesma-slobodi.php
@@ -7,24 +7,26 @@ include "../includes/header.php";
 
 <h1>Pjesma slobodi</h1>
 
+Slobodo, slobodo, slobodo!<br />
+Nisi nam došla ko s neba poslati dar.<br />
+Nije darovao tebe ni gospod, ni kralj, ni car.<br />
+<br />
+Sami smo gradili tebe u borbi patnji i krvi.<br />
+Grobovi silni govore o našoj krvavoj žrtvi.<br />
+<br />
+Slobodo!<br />
+<br />
+Same smo gradile tebe gladne gole i bose.<br />
+Zgarišta naša govore kako se žrtve podnose.<br />
+<br />
+Slobodo!<br />
+<br />
+Krvlju stečena, patnjama našim slivena.<br />
+Slobodo, izvoru sreće,<br />
+nitko nam otet te neće, nitko!<br />
+<br />
+Slobodo!<br />
+
 <p>
-  Slobodo, slobodo, slobodo!<br />
-  Nisi nam došla ko s neba poslati dar.<br />
-  Nije darovao tebe ni gospod, ni kralj, ni car.<br />
-  <br />
-  Sami smo gradili tebe u borbi patnji i krvi.<br />
-  Grobovi silni govore o našoj krvavoj žrtvi.<br />
-  <br />
-  Slobodo!<br />
-  <br />
-  Same smo gradile tebe gladne gole i bose.<br />
-  Zgarišta naša govore kako se žrtve podnose.<br />
-  <br />
-  Slobodo!<br />
-  <br /> 
-  Krvlju stečena, patnjama našim slivena.<br />
-  Slobodo, izvoru sreće,<br />
-  nitko nam otet te neće, nitko!<br />
-  <br />
-  Slobodo!<br />
+  <img src="http://www.znaci.org/images/7859.jpg" alt="">
 </p>
