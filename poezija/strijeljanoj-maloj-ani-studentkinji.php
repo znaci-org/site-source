191986abb051734e2fb0e6c1b9217f13b056cbec
Damjan Pavlica <mudroljub@gmail.com>
2024-06-20 02:27:58 +0200

    slike

--- poezija/strijeljanoj-maloj-ani-studentkinji.php
+++ poezija/strijeljanoj-maloj-ani-studentkinji.php
@@ -1,32 +1,33 @@
 <?php
-$naslov = "Poezija NOB";
+$naslov = "Radovan Zogović - Strijeljanoj maloj Ani studentkinji";
 include "../includes/header.php";
 ?>
 
 <a href="/poezija.php">◀ Poezija NOB</a>
 
-<h1>Radovan Zogović - Strijeljanoj maloj Ani studentkinji</h1>
+<h1><?php echo $naslov ?></h1>
 
-<p>
-  Mi smo voljeli vjetar po tek isklasaloj zobi,<br />
-  i snijeg gradski – po trepavicama zvjezdice i latice.<br />
-  I imali smo – čas u mojoj, čas u njenoj sobi –<br />
-  uramljenu kopiju Renoarove Čitačice.<br />
-  <br />
-  I imali smo, nas dvoje, iz skupnog bježanja od žandara,<br />
-  iz zaklona najzad, poljubac koji nijesmo ponovili.<br />
-  Neizbježni poljubac iza susreta očiju od sreće i od žara;<br />
-  nezaboravni, o kojemu nikad nijesmo govorili.<br />
-  <br />
-  I ko je znao, i ko je mogao misliti, mala Ana,<br />
-  da će poljubac naš, sa usana tvojih, postati krvav ridaj,<br />
-  a Djevojka s knjigom – kurirka partizana,<br />
-  uhvaćena kurirka što i na stratištu mirno čita.<br />
-  <br />
-  I ko bi znao, ko bi mogao misliti, mala Ana,<br />
-  da će Čitačici našoj – niz bradu, niz grlo i na čipke<br />
-  pod grlom – teći krv iz usta, nepotamnjiva, neprestana,<br />
-  krv, a ne odblijesak od lica i od knjige.<br />
-  <br />
-  Krv koju gledam i sanjam, krv tvoja, mila Ana!<br />
-</p>
+Mi smo voljeli vjetar po tek isklasaloj zobi,<br />
+i snijeg gradski – po trepavicama zvjezdice i latice.<br />
+I imali smo – čas u mojoj, čas u njenoj sobi –<br />
+uramljenu kopiju Renoarove Čitačice.<br />
+<br />
+I imali smo, nas dvoje, iz skupnog bježanja od žandara,<br />
+iz zaklona najzad, poljubac koji nijesmo ponovili.<br />
+Neizbježni poljubac iza susreta očiju od sreće i od žara;<br />
+nezaboravni, o kojemu nikad nijesmo govorili.<br />
+<br />
+I ko je znao, i ko je mogao misliti, mala Ana,<br />
+da će poljubac naš, sa usana tvojih, postati krvav ridaj,<br />
+a Djevojka s knjigom – kurirka partizana,<br />
+uhvaćena kurirka što i na stratištu mirno čita.<br />
+<br />
+I ko bi znao, ko bi mogao misliti, mala Ana,<br />
+da će Čitačici našoj – niz bradu, niz grlo i na čipke<br />
+pod grlom – teći krv iz usta, nepotamnjiva, neprestana,<br />
+krv, a ne odblijesak od lica i od knjige.<br />
+<br />
+Krv koju gledam i sanjam, krv tvoja, mila Ana!<br />
+<br>
+<img src="http://www.znaci.org/images/7601.jpg" alt=""><br>
+<small>Tri omladinke pred streljanje u Zagrebu.</small>
