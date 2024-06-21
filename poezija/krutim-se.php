0177d804fd2e045a692f134a5db68c1d7a84d2a3
Damjan Pavlica <mudroljub@gmail.com>
2024-06-21 20:22:29 +0200

    Krutim se

---
+++ poezija/krutim-se.php
@@ -0,0 +1,31 @@
+<?php
+$naslov = "Vladimir Nazor - Krutim se";
+include "../includes/header.php";
+?>
+
+<a href="/poezija.php">◀ Poezija NOB</a>
+
+<h1><?php echo $naslov ?></h1>
+
+Krutim se, sušim, mršavim — al' ipak,<br/>
+osjećam, gola dok postajem kost,<br/>
+da bivam sanja pun k'o zrnja šipak<br/>
+i uvis svinut k'o nad r'jekom most.<br/>
+<br/>
+Živote, spruži svoju tvrdu ruku!<br/>
+I sažmi me! — Moj sok je sada jak;<br/>
+mosta je moga na najvišem luku<br/>
+svjet čitav dao mnogih sunca trak.<br/>
+<br/>
+Pšenično zrno il' pljeve trun (svejedno),<br/>
+nek s vihorom se bratimim! Nek strt<br/>
+u prah — a ne znam da li slavno il b'jedno —<br/>
+dočekam u toj olujini smrt:<br/>
+<br/>
+smrt bogatu što u tren jedan plaća<br/>
+godina škrtih prikraćeni dug:<br/>
+smrt koja, dok vam ukradeno vraća,<br/>
+prolazi kroz nas k'o po njivi plug.<br/>
+<br/>
+(Rudnik Tšina, kod Livna<br/>
+30 V 1943)
