345281e5ef4bdc59d7f9b361b73bd6acb7d2015c
Damjan Pavlica <mudroljub@gmail.com>
2024-07-13 12:45:04 +0200

    stane

--- poezija/komandant-stane.php
+++ poezija/komandant-stane.php
@@ -9,23 +9,31 @@ include "../includes/header.php";
 
 <p>(<i>slovenska narodna pesem</i>)</p>
 
-Brigade s hribov so se vsule kakor plaz,<br/>
-pogumni njih korak nikjer več ne zastane.<br/>
-Za naš pravični boj, sovražniku v poraz<br/>
-je borcem dala vsaka naša vas,<br/>
-na čelu vseh je v boj hitel tovariš Stane.<br/>
-<br/>
-Je že nasilnik na telesu našem stal,<br/>
-globoko nade vse bile so zakopane.<br/>
-Le naj slovenski rod s ujetim puntom vstal<br/>
-in z lastno silo novo vojsko zbral,<br/>
-pognal v naskok se z nami je tovariš Stane.<br/>
-<br/>
-Zastave vojne, počastite mu spomin,<br/>
-potem zvihrajte v boj z armado do Ljubljane!<br/>
-In ves slovenski rod, hvaležno posveti<br/>
-junakom svojim naj slavi, časti,<br/>
-od zmag do zmag nas vodil je tovariš Stane!<br/>
-<br>
+Brigade s hribov so se vsule kakor plaz,<br />
+pogumni njih korak nikjer več ne zastane.<br />
+Za naš pravični boj, sovražniku v poraz<br />
+je borcem dala vsaka naša vas,<br />
+na čelu vseh je v boj hitel tovariš Stane.<br />
+<br />
+Je že nasilnik na telesu našem stal,<br />
+globoko nade vse bile so zakopane.<br />
+Le naj slovenski rod s ujetim puntom vstal<br />
+in z lastno silo novo vojsko zbral,<br />
+pognal v naskok se z nami je tovariš Stane.<br />
+<br />
+Zastave vojne, počastite mu spomin,<br />
+potem zvihrajte v boj z armado do Ljubljane!<br />
+In ves slovenski rod, hvaležno posveti<br />
+junakom svojim naj slavi, časti,<br />
+od zmag do zmag nas vodil je tovariš Stane!<br />
 
-<img src="https://upload.wikimedia.org/wikipedia/commons/c/c6/Franc_Rozman-Stane.jpg" alt="">
+<p>(autori pesme su Cvetko Zagorski i Mile Klopčič, muzika Karol Pahor)</p>
+
+<p>
+  <img src="skenovi/Komandant Stane 1.jpg" alt="">
+  <img src="skenovi/Komandant Stane 2.jpg" alt="">
+</p>
+
+<p>
+  <img src="https://upload.wikimedia.org/wikipedia/commons/c/c6/Franc_Rozman-Stane.jpg" alt="">
+</p>
