680565911341e0b3e7d914b51271002e5cd290bb
Damjan Pavlica <mudroljub@gmail.com>
2024-07-15 22:50:41 +0200

    seljacka

--- poezija/puntarska.php
+++ poezija/puntarska.php
@@ -7,35 +7,38 @@ include "../includes/header.php";
 
 <h1><?php echo $naslov ?></h1>
 
-<p>(napev iz časa bojev za Staro Pravdo)</p>
-
-Le vkup, le vkup, uboga gmajna,<br/>
-heja, hejo!<br/>
-Za staro pravdo zdaj bo drajna,<br/>
-heja, hejo!<br/>
-Zimzelen za klobuk!<br/>
-Punt naj reši nas tlačanskih muk<br/>
-<br/>
-Le vkup, v poslednji boj, tlačani,<br/>
-heja, hejo!<br/>
-Sedaj se kmečka gmajna brani,<br/>
-heja, hejo!<br/>
-Puško, meč, kopje v dlan!<br/>
-Bije za svobodo se tlačan!<br/>
-<br/>
-Iz grajskih kevdrov teče vino,<br/>
-heja, hejo!<br/>
-Zažgali grofu smo graščino,<br/>
-heja, hejo!<br/>
-Grad gori, grof beži,<br/>
-vino teče naj, če teče kri!<br/>
+Le vkup, le vkup, uboga gmajna,<br />
+heja, hejo!<br />
+Za staro pravdo zdaj bo drajna,<br />
+heja, hejo!<br />
+Zimzelen za klobuk!<br />
+Punt naj reši nas tlačanskih muk<br />
+<br />
+Le vkup, v poslednji boj, tlačani,<br />
+heja, hejo!<br />
+Sedaj se kmečka gmajna brani,<br />
+heja, hejo!<br />
+Puško, meč, kopje v dlan!<br />
+Bije za svobodo se tlačan!<br />
+<br />
+Iz grajskih kevdrov teče vino,<br />
+heja, hejo!<br />
+Zažgali grofu smo graščino,<br />
+heja, hejo!<br />
+Grad gori, grof beži,<br />
+vino teče naj, če teče kri!<br />
 
 <p>(1936.)</p>
 
-<img src="skenovi/seljacka.jpg" alt="">
+<h2>Seljačka (Mi smo Gupca muške čete)</h2>
+
+<p>(hrvatskosrpski prepev)</p>
+
+<p>
+  <img src="skenovi/Seljacka 1.jpg" alt=""><br>
+  <img src="skenovi/Seljacka 2.jpg" alt="">
+</p>
 
 <hr>
 
-<p>»Le vkup, le vkup uboga gmajna!« in »Stara pravda!« so v začetku 16. stoletja vzklikali slovenski kmetje v uporih proti fevdalni gospodi. Ta dva zgodovinska okruška sta bila podlaga pesniku Miletu Klopčiču za pesem Puntarska, ki jo je leta 1936 napisal za dramo Velika puntarija Bratka Krefta.</p>
-
-<p>Seljačka (Mi smo Gupca muške čete), prepev stare nemačke, s temom seljačke bune i melodijom iz njemačkog seljačkog rata.</p>
+<p>Muzika je preuzeta od nemačke pesme iz 1920-ih, <i>Wir sind des Geyers schwarzer Haufen</i> (Mi smo Gejerove crne čete), posvećene nemačkoj seljačkoj buni iz 1524. godine.</p>
