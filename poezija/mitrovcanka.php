4c2eac9671e7941c60789e902e6ef5cb823ba1ea
Damjan Pavlica <mudroljub@gmail.com>
2024-07-15 19:51:45 +0200

    note

--- poezija/mitrovcanka.php
+++ poezija/mitrovcanka.php
@@ -55,13 +55,22 @@ A kad dođe dan slobode<br />
 mitrovčanku pevat ćemo<br />
 bez okova naših.<br />
 
-<p><img src="skenovi/mitrovcanka.jpg" alt=""></p>
+<p>
+  <img src="skenovi/Mitrovcanka 1.jpg" alt=""><br>
+  <img src="skenovi/Mitrovcanka 2.jpg" alt=""><br>
+  <img src="skenovi/Mitrovcanka 3.jpg" alt=""><br>
+</p>
 
 <hr>
 
-<p>Mnoge revolucionarne pjesme spjevane su u kaznionicama za političke zatvorenike u Bileći, Sremskoj Mitrovici i Lepoglavi. Zbog uvjeta u kojima su nastale, ove pjesme se popularno nazivaju robijaškim. Nastajale su spontano i najčešće im se ne može odrediti autor, jer je u pisanju teksta učestvovalo više zatvorenika, a katkada cijela ćelija ili paviljon.</p>
+<p>Mnoge revolucionarne pjesme spjevane su u kaznionicama za političke zatvorenike u Bileći, Sremskoj Mitrovici i
+  Lepoglavi. Zbog uvjeta u kojima su nastale, ove pjesme se popularno nazivaju robijaškim. Nastajale su spontano i
+  najčešće im se ne može odrediti autor, jer je u pisanju teksta učestvovalo više zatvorenika, a katkada cijela ćelija ili paviljon.</p>
 
-<p>U kaznioni u Sremskoj Mitrovici je 1936. godine organiziran Prvi festival originalnih revolucionarnih pjesama na kojem su učestvovali svi sobni kolektivi. Od većeg broja konkurenata pobijedila je slovenačka grupa sa pjesmom Žetelice (kasnije preimenovana u Nabrusimo kose), posebnu nagradu žirija dobila je Mitrovčanka, a istom prilikom je nastala i Pjesma o Matiji Gupcu. </p>
+<p>U kaznioni u Sremskoj Mitrovici je 1936. godine organiziran Prvi festival originalnih revolucionarnih pjesama na
+  kojem su učestvovali svi sobni kolektivi. Od većeg broja konkurenata pobijedila je slovenačka grupa sa pjesmom
+  Žetelice (kasnije preimenovana u Nabrusimo kose), posebnu nagradu žirija dobila je Mitrovčanka, a istom prilikom je nastala i Pjesma o Matiji Gupcu. </p>
 
-<img src="https://upload.wikimedia.org/wikipedia/commons/7/75/Politi%C4%8Dki_zatvorenici_u_Sremskoj_Mitrovici_1930-ih.jpg"><br>
+<img
+  src="https://upload.wikimedia.org/wikipedia/commons/7/75/Politi%C4%8Dki_zatvorenici_u_Sremskoj_Mitrovici_1930-ih.jpg"><br>
 <small>Politički zatvorenici u Sremskoj Mitrovici 1930-ih.</small>
