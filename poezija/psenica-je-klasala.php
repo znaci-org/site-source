df5b0e3f9b875157256ec24f41eddf8ebda22db8
Damjan Pavlica <mudroljub@gmail.com>
2024-07-16 21:28:33 +0200

    Pšenica je klasala

--- poezija/psenica-je-klasala.php
+++ poezija/psenica-je-klasala.php
@@ -9,6 +9,11 @@ include "../includes/header.php";
 
 <p>(<i>srpska narodna pesma</i>)</p>
 
+Pšenica je klasala,<br/>
+Šumadija ustala.<br/>
+Šumadija, naša nana,<br/>
+puna partizana<br/>
+<br/>
 Pšenica je klasala<br/>
 Šumadija ustala<br/>
 Šumadija naša dika<br/>
@@ -19,12 +24,20 @@ Pšenica je klasala,<br/>
 Šumadija naša dika<br/>
 Biće republika<br/>
 
-<h2>II verzija (Šenica je klasala)</h2>
+<h2>II verzija</h2>
+
+Pšenica je klasala,<br/>
+Šumadija glasala,<br/>
+Šumadija naša nana,<br/>
+Puna partizana <br/>
+
+<h2>III verzija (Šenica je klasala)</h2>
 
 Šenica je klasala,<br/>
 Jablanica ustala,<br/>
 Jablanica naša nana<br/>
 Puna partizana.<br/>
+<br/>
 Koliko je u šumici grana,<br/>
 Toliko je mladih partizana.<br/>
 Koliko je u šumici lista,<br/>
@@ -33,3 +46,7 @@ Još je više mladih komunista.<br/>
 <p>(prvi pevana u novembru 1941. godine u Jablanici, prilikom zauzimanja Lebana)</p>
 
 <p>(Sergije Dimitrijević, Crnotravske i leskovačke narodne pesme oslobodilačkog rata i revolucije, Beograd, 1967., str. 77.)</p>
+
+<hr>
+
+<p>Pesma je pevana u ustaničkoj Srbiji 1941. Prvu verziju pesme (bar jednu strofu) spevao je drug Sreten Žujović.</p>
