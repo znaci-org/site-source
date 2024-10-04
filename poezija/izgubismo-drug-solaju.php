67af21d6bc4051fda7577b02502eb49e662364ca
Damjan Pavlica <mudroljub@gmail.com>
2024-10-04 21:13:59 +0200

    Izgubismo drug Šolaju

---
+++ poezija/izgubismo-drug-solaju.php
@@ -0,0 +1,23 @@
+<?php
+$naslov = "Izgubismo drug Šolaju";
+include "../includes/header.php";
+?>
+
+<a href="/poezija.php">◀ Poezija NOB</a>
+
+<h1><?php echo $naslov ?></h1>
+
+<p>(<i>narodna pjesma</i>)</p>
+
+<pre>
+U nedelju prije zore  
+partizanski bacač ore,  
+
+bacač ore, topi tresu –  
+to je borba na Kupresu.  
+
+Na kupreškom položaju  
+izgubismo drug Šolaju.  
+</pre>
+
+<p>(zabeležio Mirko Raković)</p>
---
+++ poezija/pjesma-vladi-segrtu.php
@@ -0,0 +1,23 @@
+<?php
+$naslov = "Pjesma Vladi Šegrtu";
+include "../includes/header.php";
+?>
+
+<a href="/poezija.php">◀ Poezija NOB</a>
+
+<h1><?php echo $naslov ?></h1>
+
+<p>(<i>narodna pjesma</i>)</p>
+
+<pre>
+Drugarice, b'jela Rado,  
+je li kakav Šegrt Vlado?  
+
+Srednjeg rasta, bradu brije,  
+komandant je divizije.  
+
+Brije bradu i brkove,  
+s bombom skače na tenkove.  
+</pre>
+
+<p>(zabeležio Mirko Raković)</p>
