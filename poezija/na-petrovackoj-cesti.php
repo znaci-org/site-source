819627952b934e0d4bfc9be9d888bed159a2ff7b
Damjan Pavlica <mudroljub@gmail.com>
2025-06-25 09:26:55 +0200

    Update na-petrovackoj-cesti.php

--- poezija/na-petrovackoj-cesti.php
+++ poezija/na-petrovackoj-cesti.php
@@ -10,69 +10,81 @@ include "../includes/header.php";
 <p>
   Na cesti petrovačkoj izbjeglice<br />
   i trista djece u koloni,<br />
-  nad cestom kruže grabljive ptice –<br />
+  nad cestom kruže grabljivice,<br />
   tuđinski avioni.<br />
   Po kamenjaru osniježenom<br />
   čelična kiša zvoni…<br />
-  U snijegu mrtva Marija,<br />
+  <br />
+  U snijegu rumenu Marija,<br />
   mamina kćerka jedina,<br />
   bilo joj je sedam godina.<br />
-  Tri dana snijeg je gazila<br />
-  i posrnula stotinu puta,<br />
-  suknju je imala ni kratku ni dugu<br />
-  i prsluk malen, premalen,<br />
-  a povrh svega kabanica<br />
-  beskrajnih rukava, široka, žuta,<br />
+  <br />
+  Tri dana Grmeč gazila<br />
+  i posrnula stotinu puta.<br />
+  Suknju je imala - ni kratku ni dugu,<br />
+  i prsluk - malen, premalen,<br />
+  a povrh svega kabanica,<br />
+  beskrajnih rukava, široka žuta,<br />
   od starog očevog kaputa.<br />
+  <br />
   Ponekad mala plakala.<br />
-  a sad se opet smijala<br />
+  a nekad se opet smijala<br />
   i vesela bila,<br />
   kad bi je mati tješila:<br />
+  <br />
   Još samo malo, rođena,<br />
   pa ćemo vidjeti Petrovac,<br />
   a to je varoš golema,<br />
-  tu ima vatre i ‘ljeba<br />
+  tu ima vatre i hljeba<br />
   i kuća – do samoga neba.<br />
+  <br />
   Radovala se djevojčica<br />
-  i vatri i gradu neviđenom,<br />
+  i vatri, i gradu neviđenom,<br />
   a sada leži, sićušna kao ptica,<br />
   na cesti Petrovačkoj,<br />
   na cesti okrvavljenoj.<br />
+  <br />
   Oči gledaju širom, al sjaja u njima nema,<br />
-  sa mrtvih usana male optužba teče nijema.<br />
+  sa mrtvih usana male optužba teče nijema:<br />
+  <br />
   O, strašna ptico, ti si me ubila,<br />
-  a što sam ti kriva bila?<br />
+  a što sam ti kriva bila!<br />
+  <br />
   Sedam sam godina imala,<br />
-  ni mrava nisam zgazila;<br />
-  tako sam malo živjela,<br />
-  i tako malo vidjela<br />
+  ni mrava nisam zgazila.<br />
+  Tako sam malo živjela,<br />
+  i tako malo vidjela,<br />
   a svemu sam se divila.<br />
-  Bila sam bezbrižni leptir, a ti me pokosi, ptico,<br />
-  ti mi ugasi zjene, polomi ručice moje od gladi otežale,<br />
-  od zime ukočene…<br />
-  Optužbu diže dijete, zgrčenih sićušnih pesti,<br />
-  u okrvavljenom snijegu na Petrovačkoj cesti…<br />
+  Bila sam bezbrižni leptir,<br />
+  a ti me pokosi, ptico,<br />
+  ti mi ugasi zjene,<br />
+  polomi ručice moje od gladi otežale,<br />
+  od zime ukočene.<br />
+  <br />
+  Optužbu vapije dijete, zgrčenih sićušnih pesti,<br />
+  u okrvavljenom snijegu na Petrovačkoj cesti.<br />
+  <br />
   Tuđinski ljudi krvavi<br />
   kuću su našu spalili,<br />
-  djetinjstvo su mi ukrali.<br />
-  Tuđinske ptice, gvozdene, nemile,<br />
-  nad Grmečom su našim letjele.<br />
-  Planino moja visoka, planino ponosna, mila,<br />
-  tek sam sedam godina imala<br />
-  i tek sam putom prohodala,<br />
-  a tebe sam u zimskoj noći pregazila.<br />
+  djetinjstvo su mi ukrali<br />
+  i mnogo naših ubili.<br />
+  <br />
+  Tuđinske ptice, čelične nemile,<br />
+  nad planinom su našom letile.<br />
+  <br />
   Smrači se, rođena goro, i na sve naše pute<br />
-  pošalji sinove svoje i osvetnike ljute.<br />
-  Osveti moje noge izranjene<br />
-  i jutra gladna isplakana<br />
+  pošalji sinove svoje, pošalji vukove ljute,<br />
+  osveti moje noge izranjene,<br />
+  i jutra gladna rasplakana<br />
   i ruke modre i smrznute.<br />
-  Zagrmi, tata, iz velikog topa,<br />
+  <br />
+  Zagrmi tata iz velikog topa,<br />
   pomlati tuđe gadove,<br />
-  zabubnjaj, braco, iz mitraljeza<br />
-  mrtva te sestra zove…<br />
-  Optužbu vapije dijete, stisnutih modrih pesti,<br />
-  u krvi i snijegu na<br />
-  Petrovačkoj cesti.<br />
+  zabubnjaj, braco, mitraljezom<br />
+  mrtva te sestra zove.<br />
+  <br />
+  Optužbu vapije dijete, stisnutih smrznutih pesti,<br />
+  u krvi i snijegu na Petrovačkoj cesti.<br />
 </p>
 
 (1944.)
